5d832797b0e00632b3af0e7a269aa3cd03f1cf2d
tsuarabh <114585130+tsuarabh@users.noreply.github.com>
2024-02-03 16:29:43 -0800

    Updated test2.sh

--- Homeworks/HW 1/test2.sh
+++ Homeworks/HW 1/test2.sh
@@ -17,9 +17,9 @@ echo "Running Second File I/O Test: Random Read"
 for i in {1..5}
 do
     echo "Iteration $i"
-    sysbench --test=fileio --file-total-size=250M --file-test-mode=rndrd prepare
-    sysbench --test=fileio --file-total-size=250M --file-test-mode=rndrd run
-    sysbench --test=fileio --file-total-size=250M cleanup
+    sysbench --test=fileio --file-total-size=300M --file-test-mode=rndrd prepare
+    sysbench --test=fileio --file-total-size=300M --file-test-mode=rndrd run
+    sysbench --test=fileio --file-total-size=300M cleanup
     echo ""
 done
 
